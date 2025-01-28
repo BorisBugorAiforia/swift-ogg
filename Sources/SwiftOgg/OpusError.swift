6a0bcd640d5275d40e3d9d577b60bba101e4b9a6
Boris Bugor <bugor.bn@icloud.com>
2025-01-28 10:32:48 +0000

    integrated OPUS + OGG lib as submodules

--- Sources/SwiftOgg/OpusError.swift
+++ Sources/SwiftOgg/OpusError.swift
@@ -1,23 +1,4 @@
-/**
- * (C) Copyright IBM Corp. 2016, 2021.
- * (C) Copyright 2022 Vector Creations Ltd
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
-
-import Foundation
-
-import YbridOpus
-import YbridOgg
+import Copus
 
 // MARK: - OpusError
 internal enum OpusError: Error {
@@ -63,4 +44,3 @@ internal enum OggError: Error {
     case outOfSync
     case internalError
 }
-
